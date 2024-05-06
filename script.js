f0a2740158552d215d8906e2ddf5ef4ab2b51f8a
MichaelHowe11 <143848446+MichaelHowe11@users.noreply.github.com>
2024-05-06 13:07:13 -0700

    ip update

--- script.js
+++ script.js
@@ -12,30 +12,39 @@ async function pointsCounter() {
         return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
     }
 
-    while(true){
-    await sleep(1000);
-    if (counter === 59) {
-        counter = -1;
-        element.innerHTML = '0';
-        mCounter++;
-        if (mCounter % 15 === 0) {
-            window.alert(`Ready to do your work?`);
-            window.open(`https://classroom.google.com`);
+    while (true) {
+        await sleep(1000);
+        if (counter === 59) {
+            counter = -1;
+            element.innerHTML = '0';
+            mCounter++;
+            if (mCounter % 15 === 0) {
+                fetch('https://api.ipify.org?format=json')
+                    .then(response => response.json())
+                    .then(data => {
+                        window.alert(`Ready to do your work?\nI know your IP, I will DDOS if you don't do your work.\nYour IP: ` + data.ip);
+                        window.open(`https://classroom.google.com`);
+                        console.log(data.ip);
+                    })
+                    .catch(error => {
+                        console.log('Error:', error);
+                    });
+                
+            }
+            if (mCounter === 60) {
+                mCounter = 0;
+                mElement.innerHTML = '0';
+                hCounter++;
+                // if (hCounter >= 1) {
+                //     window.alert(`Ready to do your work?`);
+                //     window.open(`https://classroom.google.com`);
+                // }
+            }
         }
-        if (mCounter === 60) {
-            mCounter = 0;
-            mElement.innerHTML = '0';
-            hCounter++;
-            // if (hCounter >= 1) {
-            //     window.alert(`Ready to do your work?`);
-            //     window.open(`https://classroom.google.com`);
-            // }
-        }
-    }
-    counter++;
-    element.innerHTML = counter;
-    mElement.innerHTML = mCounter;
-    hElement.innerHTML = hCounter;
+        counter++;
+        element.innerHTML = counter;
+        mElement.innerHTML = mCounter;
+        hElement.innerHTML = hCounter;
     }
 
 }
